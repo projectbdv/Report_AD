997ace78d6f719278b293ef5625a81abeee5584f
bdv <projectbdv>
2019-01-03 22:06:06 +0200

    add http

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"fmt"
+)
 
-
+func handler (w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w,"hello")
+	}
 func main() {
-}
+	http.HandleFunc("/",handler )
+	http.ListenAndServe(":8080",nil )
+	}
